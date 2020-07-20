9579b16bfbd1fe0917a34e7120467d56074985f1
dkim61 <dkim187@binghamton.edu>
2020-07-20 09:40:48 -0400

    attempt1

--- author.rb
+++ author.rb
@@ -1,1 +1,40 @@
+class Author
+    attr_accessor :total_words, :most_words
+    attr_reader :name  
+    @@all
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select {|book| book.author == self}
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        total_words = 0
+        books.map do |book|
+           total_words << book.word_count
+        end
+        total_words
+    end
+
+
+    def self.most_words
+        most_words = 0
+        books.each do |book|
+            if book.word_count > most_words
+                most_words = book.word_count
+            end
+        end
+        most_words
+    end
 
--- book.rb
+++ book.rb
@@ -1,1 +1,17 @@
+class Book
+    attr_accessor :title, :author, :word_count
+    @@all = []
+    def initialize(title, author, word_count)
+        @title = title
+        @author = author
+        #holds a reference to the "to many (which is the author in this case"
+        @word_count = wordcount
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
 
